4a04d120281e1d0bf634311ab279faa0ee3ccd66
marwa eldeeb <125557487+MarwaEl-deeb@users.noreply.github.com>
2025-09-13 20:16:20 +0300

    Marwa

--- Movies Application/src/Pages/TVShowsHomePage.jsx
+++ Movies Application/src/Pages/TVShowsHomePage.jsx
@@ -5,41 +5,57 @@ import Pagination from "../Components/Pagination";
 import { MainContext } from "../useContext";
 
 function TVShowsHomePage() {
-    const { setSelectedType } = useContext(MainContext);
-    const [tvItems, setTvItems] = useState([]);
+    const [items, setItems] = useState([]);
     const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
+    const [selectedType, setSelectedType] = useState("tv");
+    const { setSelectedType: setGlobalSelectedType } = useContext(MainContext);
 
     useEffect(() => {
-        setSelectedType("tv");
-    }, [setSelectedType]);
+        setGlobalSelectedType("tv");
+    }, [setGlobalSelectedType]);
 
     useEffect(() => {
         setLoading(true);
-
         fetch(
             `https://api.themoviedb.org/3/tv/popular?api_key=dd1481c9866799f1bc15adf106a083fe&page=${page}`
         )
             .then((res) => res.json())
             .then((data) => {
-                setTvItems(data.results || []);
-                setTotalPages(data.total_pages);
+                setItems(data.results || []);
+                setTotalPages(data.total_pages || 1);
                 setLoading(false);
             })
             .catch(() => setLoading(false));
-    }, [page]);
+    }, [page, selectedType]);
 
     if (loading) {
-        return <p>Loading TV Shows...</p>;
+        return (
+            <div className="loading">
+                <div className="loading"></div>
+            </div >
+        );
     }
 
     return (
-        <div>
+        <MainContext.Provider
+            value={{
+                items,
+                setItems,
+                totalPages,
+                page,
+                setPage,
+                loading,
+                setLoading,
+                selectedType,
+                setSelectedType,
+            }}
+        >
             <Welcome />
-            <CardList data={tvItems} />
+            <CardList />
             <Pagination />
-        </div>
+        </MainContext.Provider>
     );
 }
 
