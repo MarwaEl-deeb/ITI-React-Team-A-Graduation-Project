9e5f832eedfeff1ce709fde801d4a69f10a7eb2c
Hagar Ramadan <hagarramadan402@gmail.com>
2025-09-13 21:14:35 +0300

    hagar

--- Movies Application/src/App.jsx
+++ Movies Application/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Welcome from "./Components/Welcome";
 import NavBar from "./Components/NavBar";
 import CardList from "./Components/Card";
+import PaginationComponent from "./Components/Pagination";
 import { MainContext } from "./useContext";
 
 
@@ -13,6 +14,7 @@ function App() {
   const [selectedType, setSelectedType] = useState("movies");
 
   useEffect(() => {
+      const endpoint =
       selectedType === "movies"
         ? `https://api.themoviedb.org/3/movie/now_playing?api_key=dd1481c9866799f1bc15adf106a083fe&page=${page}`
         : `https://api.themoviedb.org/3/tv/popular?api_key=dd1481c9866799f1bc15adf106a083fe&page=${page}`;
@@ -22,10 +24,10 @@ function App() {
       .then((data) => {
         setItems(data.results);
         setTotalPages(data.total_pages);
+        setLoading(false) 
       })
       .catch((err) => {
         console.error(err);
-        setLoading(false);
       
       });
   }, [page, selectedType]);
