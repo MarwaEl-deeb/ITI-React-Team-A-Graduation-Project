62dd5ae56274f69ba43fe41cd054349988d039fe
Hagar Ramadan <hagarramadan402@gmail.com>
2025-09-14 20:06:23 +0300

    hagar

--- Movies Application/src/Components/Pagination.jsx
+++ Movies Application/src/Components/Pagination.jsx
@@ -1,48 +1,49 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { MainContext } from "../useContext"
 import Pagination from 'react-bootstrap/Pagination';
 
 
 function PaginationComponent() {
 
-    
-    const { totalPages , setTotalPages, setPage, page,selectedType } = useContext(MainContext);
 
+    const { totalPages, setTotalPages, setPage, page, selectedType } = useContext(MainContext);
+    let maxPages =selectedType==="movies"? totalPages: 500
     const window = 6;
     const start = Math.max(1, page - 2)
-    const end = Math.min(totalPages, start + window-1 )
+    const end = Math.min(maxPages, start + window-1);
+
     let Pages = [];
     for (let i = start; i < end; i++) {
         Pages.push
             (<Pagination.Item
                 key={i}
                 onClick={() => { setPage(i); console.log(i); }}
-                active={i===page}>
-                   {i}
+                active={i === page}>
+                {i}
             </Pagination.Item>)
     }
     return (
         <Pagination className="  w-75 justify-content-center ">
-             <Pagination.First className="paginationBtn"
-              disabled={page===1}
-               onClick={() => { setPage(1) }}/>
+            <Pagination.First className="paginationBtn"
+                disabled={page === 1}
+                onClick={() => { setPage(1) }} />
             <Pagination.Prev className="paginationBtn"
-             disabled={page===1} 
-             onClick={() => { setPage(page-1) }} />
+                disabled={page === 1}
+                onClick={() => { setPage(page - 1) }} />
 
             {start > 1 && <Pagination.Ellipsis
-             disabled={page===1} 
-             className="paginationBtn"/>}
+                disabled={page === 1}
+                className="paginationBtn" />}
             {Pages}
-            {end < totalPages && <Pagination.Ellipsis 
-             disabled={page===totalPages} 
-             className="paginationBtn"/>}
+            {end < totalPages && <Pagination.Ellipsis
+                disabled={page === totalPages}
+                className="paginationBtn" />}
             <Pagination.Next className="paginationBtn"
-             disabled={page===totalPages}
-              onClick={() => { setPage(page+1) }}/>
-            <Pagination.Last className="paginationBtn" 
-            disabled={page===totalPages} 
-            onClick={() => { setPage(totalPages) }} />
+                disabled={page === totalPages}
+                onClick={() => { setPage(page + 1) }} />
+            <Pagination.Last className="paginationBtn"
+                disabled={page === maxPages}
+                onClick={() => { setPage(maxPages) }} />
         </Pagination >
     )
 
--- Movies Application/src/Pages/ErrorPage.jsx
+++ Movies Application/src/Pages/ErrorPage.jsx
@@ -1,10 +1,14 @@
+import {  useNavigate } from "react-router-dom";
 
 function ErrorPage(){
+
+const navigate=useNavigate();
   return(
- <div className="d-flex justify-content-center align-items-center  "
+ <div className="d-flex justify-content-center align-items-center flex-column "
       style={{height:"100vh"}}>
         <h1 style={{color:'red'}}>Oops!</h1>
-        <p>page is not found</p>
+        <p>page is not found Please Go Back</p>
+        <button onClick={()=>navigate(-1)}>GO back</button>
       </div>)
 }
 export default ErrorPage;
