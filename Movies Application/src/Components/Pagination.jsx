d3de16fbe29ba7f16cdb71943de7b2add8401512
Hagar Ramadan <hagarramadan402@gmail.com>
2025-09-16 01:22:00 +0300

    hagar

--- Movies Application/src/Components/Pagination.jsx
+++ Movies Application/src/Components/Pagination.jsx
@@ -1,47 +1,62 @@
 import { useContext, useEffect } from "react"
 import { MainContext } from "../useContext"
 import Pagination from 'react-bootstrap/Pagination';
+import { useSearchParams } from "react-router-dom";
 
 
 function PaginationComponent() {
 
 
     const { totalPages, setTotalPages, setPage, page, selectedType } = useContext(MainContext);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    useEffect(() => {
+        let pageURL = parseInt(searchParams.get("page") || "1", 10)
+        if (!isNaN(pageURL) && pageURL !== page) {
+            setPage(pageURL)
+        }
+    }, [])
+
+    useEffect(() => {
+        setSearchParams({ page: page.toString() })
+    }, [page, searchParams]);
+
     let maxPages = selectedType === "movies" ? totalPages : 500
-    const window = 6;
+    const window = 4;
     const start = Math.max(1, page - 2)
     const end = Math.min(maxPages, start + window - 1);
 
     let Pages = [];
     for (let i = start; i <= end; i++) {
         Pages.push
-            (<Pagination.Item
+            (<Pagination.Item  className="col-*"
                 key={i}
                 onClick={() => { setPage(i); console.log(i); }}
                 active={i === page}>
                 {i}
+               
             </Pagination.Item>)
     }
     return (
-        <Pagination className="  w-75 justify-content-center ">
-            <Pagination.First className="paginationBtn"
+        <Pagination className=" justify-content-center gap-2">
+            <Pagination.First className=" col-* paginationBtn"
                 disabled={page === 1}
                 onClick={() => { setPage(1) }} />
 
-            <Pagination.Prev className="paginationBtn"
+            <Pagination.Prev className=" col-* paginationBtn"
                 disabled={page === 1}
                 onClick={() => { setPage(page - 1) }} />
             {start > 1 && <Pagination.Ellipsis
                 disabled={page === 1}
-                className="paginationBtn" />}
+                className=" col-* ellipsis" />}
             {Pages}
             {end < maxPages && <Pagination.Ellipsis
                 disabled={page === maxPages}
-                className="paginationBtn" />}
-            <Pagination.Next className="paginationBtn"
+                className="col-* ellipsis" />}
+            <Pagination.Next className=" col-1 paginationBtn"
                 disabled={page === maxPages}
                 onClick={() => { setPage(page + 1) }} />
-            <Pagination.Last className="paginationBtn"
+            <Pagination.Last className="col-* paginationBtn"
                 disabled={page === maxPages}
                 onClick={() => { setPage(maxPages) }} />
         </Pagination >
